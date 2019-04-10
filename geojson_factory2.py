bdcc2bb9f101971e17242c0a2a23e8489c332edc
davi <dvdgmf@gmail.com>
2019-04-10 11:40:07 -0300

    adicionando funcionalidade de lista em javascript

--- geojson_factory2.py
+++ geojson_factory2.py
@@ -18,28 +18,36 @@ addressPoints = []
 for i, tec in enumerate(tec_pandas_list):
     addressPoints.append([latlonlist[i][0], latlonlist[i][1], tec])
 
+# Removing -1 TEC values
+addressPoints = [item for item in addressPoints if item[2] != -1]
 
-def data2geojson(df):
-    features = []
-    insert_features = lambda X: features.append(
-            geojson.Feature(geometry=geojson.Point((X["lon"],
-                                                    X["lat"])),
-                            properties=dict(tec=X["tec"])))
-    df.apply(insert_features, axis=1)
-    with open('tec-amap.geojson', 'w', encoding='utf8') as fp:
-        geojson.dump(geojson.FeatureCollection(features), fp, sort_keys=True, ensure_ascii=False)
+# Saving as a javascript list
+with open('Silvao.js', 'w') as myfile:
+    myfile.write('export const addressPoints = ' + str(addressPoints))
 
 
-col = ['lat', 'lon', 'tec']
-
-data = addressPoints
-drop = [d for d in data if d[2] != -1]
-
-print(data[:10],'\n\n',drop[:10])
-# data = [[-29.9953, -70.5867, 760],
-#         [-30.1217, -70.4933, 1250],
-#         [-30.0953, -70.5008, 1185]]
-
-df = pd.DataFrame(drop, columns=col)
-
-data2geojson(df)
+# UNCOMENT THESE FOR GEOJson files
+# def data2geojson(df):
+#     features = []
+#     insert_features = lambda X: features.append(
+#             geojson.Feature(geometry=geojson.Point((X["lon"],
+#                                                     X["lat"])),
+#                             properties=dict(tec=X["tec"])))
+#     df.apply(insert_features, axis=1)
+#     with open('tec-amap.geojson', 'w', encoding='utf8') as fp:
+#         geojson.dump(geojson.FeatureCollection(features), fp, sort_keys=True, ensure_ascii=False)
+#
+#
+# col = ['lat', 'lon', 'tec']
+#
+# data = addressPoints
+# drop = [d for d in data if d[2] != -1]
+#
+# print(data[:10],'\n\n',drop[:10])
+# # data = [[-29.9953, -70.5867, 760],
+# #         [-30.1217, -70.4933, 1250],
+# #         [-30.0953, -70.5008, 1185]]
+#
+# df = pd.DataFrame(drop, columns=col)
+#
+# data2geojson(df)
